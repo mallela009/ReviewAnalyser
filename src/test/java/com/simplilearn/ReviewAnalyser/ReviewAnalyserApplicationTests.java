61ecfa3ed58d3bb230a429329f989b7dbec44456
mallela009 <mallelaarjuna009@gmail.com>
2022-12-01 10:41:00 +0530

    add logic and test

--- src/test/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplicationTests.java
+++ src/test/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -1,13 +1,16 @@
 package com.simplilearn.ReviewAnalyser;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReviewAnalyserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
+    }
 }
+
